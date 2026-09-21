c9ac6bb5f0a594e5712c9712fdaf2523cd932fb1
mac <mac@Js-MacBook-Air.local>
2026-09-22 00:25:24 +0800

    Award task stars on every check-in

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "reward-adventure-v5";
+const CACHE_NAME = "reward-adventure-v6";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
